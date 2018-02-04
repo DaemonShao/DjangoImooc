0c830f58f70b27797ee62b591e7256cf27150fd5
daemonshao@gmail.com <daemonshao@gmail.com>
2018-02-04 14:14:59 +0800

    user_app adminx

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -3,12 +3,25 @@
 import xadmin
 
 from .models import EmailVerifyRecord
+from .models import Banner
 
 
 class EmailVerifyRecordAdmin(object):
     """
     EmailVerifyRecord注册
     """
-    pass
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    search_fields = ['code', 'email', 'send_type']
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+ 
 
+class BannerAdmin(object):
+    """
+    Banner注册
+    """
+    list_display = ["title", "image", "url", "index", "add_time"]
+    search_fields = ["title", "url", "index"]
+    list_filter = ["title", "url", "index", "add_time"]
+    
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+xadmin.site.register(Banner, BannerAdmin)
--- apps/users/models.py
+++ apps/users/models.py
@@ -15,7 +15,8 @@ class UserProfile(AbstractUser):
 
     nick_name = models.CharField(max_length=50, verbose_name=u"昵称", default="")
     birday = models.DateField(verbose_name=u"生日", null=True, blank=True)
-    gender = models.CharField(max_length=10, choices=(("male","男"),("female","女")), default=u"female")
+    gender = models.CharField(max_length=10, choices=(("male", "男"),
+                                                      ("female", "女")), default=u"female")
     address = models.CharField(max_length=100, default="")
     mobile = models.CharField(max_length=11, null=True, blank=True)
     image = models.ImageField(upload_to="image/%Y/%m", default=u"image/default.png", max_length=100)
@@ -23,7 +24,6 @@ class UserProfile(AbstractUser):
     class Meta:
         verbose_name = u"用户信息"
         verbose_name_plural = verbose_name
-    
     def __unicode__(self, ):
         return self.username
 
@@ -34,12 +34,14 @@ class EmailVerifyRecord(models.Model):
     """
     code = models.CharField(max_length=20, verbose_name=u"验证码")
     email = models.EmailField(max_length=50, verbose_name=u"邮箱")
-    send_type = models.CharField(choices=(("register",u"注册"),("forget",u"找回密码")), max_length=10)
-    send_time = models.DateTimeField(default=datetime.now, verbose_name=u"发送事时间")
+    send_type = models.CharField(choices=(("register", u"注册"), ("forget", u"找回密码")), max_length=10)
+    send_time = models.DateTimeField(default=datetime.now, verbose_name=u"发送时间")
 
     class Meta:
         verbose_name = u"邮箱验证码"
         verbose_name_plural = verbose_name
+    def __unicode__(self, ):
+        return self.code
 
 
 class Banner(models.Model):
